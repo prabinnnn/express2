7e1dd2bd7247f6a017aae1c26bbaa23f8469be83
prabinnnn <prabinbhandari120@gmail.com>
2024-01-25 17:29:17 +0545

    add index

--- route/ran.route.js
+++ route/ran.route.js
@@ -1,33 +1,54 @@
 const router = require("express").Router();
 const { passwordgenerate } = require("../utils/pass");
-router.get("/:name", (req, res) => {
-  const result = passwordgenerate(
-    req.params.name || req.query.name || req.body.name
-  );
-  res.json({ msg: result });
+router.get("/:name", (req, res, next) => {
+  try {
+    const result = passwordgenerate(
+      req.params.name || req.query.name || req.body.name
+    );
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
 });
-router.put("/:name", (req, res) => {
-  const result = passwordgenerate(
-    req.params.name || req.query.name || req.body.name
-  );
-  res.json({ msg: result });
+router.put("/:name", (req, res, next) => {
+  try {
+    ee;
+    const result = passwordgenerate(
+      req.params.name || req.query.name || req.body.name
+    );
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
 });
-router.post("/:name", (req, res) => {
-  const result = passwordgenerate(
-    req.params.name || req.query.name || req.body.name
-  );
-  res.json({ msg: result });
+router.post("/:name", (req, res, next) => {
+  try {
+    const result = passwordgenerate(
+      req.params.name || req.query.name || req.body.name
+    );
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
 });
-router.patch("/:name", (req, res) => {
-  const result = passwordgenerate(
-    req.params.name || req.query.name || req.body.name
-  );
-  res.json({ msg: result });
+router.patch("/:name", (req, res, next) => {
+  try {
+    const result = passwordgenerate(
+      req.params.name || req.query.name || req.body.name
+    );
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
 });
-router.delete("/:name", (req, res) => {
-  const result = passwordgenerate(
-    req.params.name || req.query.name || req.body.name
-  );
-  res.json({ msg: result });
+router.delete("/:name", (req, res, next) => {
+  try {
+    const result = passwordgenerate(
+      req.params.name || req.query.name || req.body.name
+    );
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
 });
 module.exports = router;
